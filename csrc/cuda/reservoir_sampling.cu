598977b7c8dc073422c3257b5ae323435a40b42b
WinticsAI <levi.viana@wintics.com>
2019-03-09 01:01:18 +0100

    Cleaning generate_samples kernel

--- csrc/cuda/reservoir_sampling.cu
+++ csrc/cuda/reservoir_sampling.cu
@@ -2,36 +2,30 @@
 
 int const threadsPerBlock = 256;
 
-__global__ void reservoir_generator_cuda(
-  int64_t *x_ptr,
-  int n,
+__global__ void generate_samples(
+  int64_t *samples,
   int k,
   curandStateMtgp32 *state
 ){
-
-  extern __shared__ int64_t samples[];
-
-  for(int i = k + 1 + blockIdx.x * blockDim.x + threadIdx.x;
-         i <= n;
-         i += blockDim.x * gridDim.x){
-
-    unsigned int z = curand(state) % i;
-    samples[threadIdx.x] = z;
-    __syncthreads();
-
-    for (int j = 0; j < threadIdx.x; j++){
-      if (samples[j] == samples[threadIdx.x]){
-        z = k + 1;
-        int _i = i - threadIdx.x + j;
-        thrust::swap(x_ptr[_i - 1], x_ptr[i - 1]);
-      }
-    }
-
-    if (z < k) {
-      thrust::swap(x_ptr[z], x_ptr[i - 1]);
-    }
+  int thread_id = blockIdx.x * blockDim.x + threadIdx.x;
+  if (thread_id >= k){
+    samples[thread_id - k] = curand(state) % (thread_id + 1);
   }
+}
 
+__global__ void generate_reservoir(
+  int64_t *indices,
+  int64_t *samples,
+  int nb_iterations,
+  int k
+){
+  for(int i = 0; i < nb_iterations; i ++){
+    int64_t z = samples[i];
+    if (z < k) {
+      thrust::swap(indices[z], indices[i + k]);
+    }
+
+  }
 }
 
 torch::Tensor reservoir_sampling_cuda(torch::Tensor& x, int k){
@@ -65,15 +59,29 @@ torch::Tensor reservoir_sampling_cuda(torch::Tensor& x, int k){
   dim3 blocks((nb_iterations + threadsPerBlock - 1)/threadsPerBlock);
   dim3 threads(threadsPerBlock);
 
-  reservoir_generator_cuda<<<blocks, threads, nb_iterations * sizeof(int64_t) >>>(
-    indices_n.data<int64_t>(),
-    n,
+  int64_t *samples_dev = 0;
+  cudaMalloc((void **)&samples_dev, nb_iterations * sizeof(int64_t));
+
+  generate_samples<<<blocks, threads>>>(
+    samples_dev,
     split,
-    generator->state.gen_states);
+    generator->state.gen_states
+  );
+
+  cudaDeviceSynchronize();
+
+  generate_reservoir<<<1, 1>>>(
+    indices_n.data<int64_t>(),
+    samples_dev,
+    nb_iterations,
+    split
+  );
 
   auto i_n = thrust::device_ptr<int64_t>(indices_n.data<int64_t>());
   auto i_k = thrust::device_ptr<int64_t>(indices_k.data<int64_t>());
   thrust::copy(i_n + begin, i_n + end, i_k);
+
+  cudaFree(samples_dev);
 
   return x.index_select(0, indices_k);
 
